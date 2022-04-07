835c251a3af62876584f7ce5ebba0d4eb606eb58
CodeOpFE <codeopfe@MacBook-Air-de-CodeOpFE-5.local>
2022-04-07 11:21:34 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1>Hola</h1>
+  <RouterView />
+</template>
+
+<script>
+export default {};
+</script>
---
+++ src/routes.js
@@ -0,0 +1,3 @@
+import Inici from "./views/Inici.vue";
+
+export const routes = [{ path: "/", name: "inici", component: Inici }];
