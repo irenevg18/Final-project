187d70ab198ffe3168e38c8fda2eea9d97e06251
Irene Valle <irenevalle@MacBook-Pro-de-Mac.local>
2022-04-10 16:52:44 +0200

    estilo p-comentaris

--- src/views/CapitolDestacat.vue
+++ src/views/CapitolDestacat.vue
@@ -1,19 +1,23 @@
 <template>
-  <h1>capitol destacat</h1>
-  <div class="flex">
-    <div class="flex flex-col">
+  <div class="flex relative">
+    <div class="flex bg-black/50 w-screen absolute pt-28 gap-2 items-center -left-[718px] -top-32 flex-col">
+      <h1 class="text-center text-white pt-4 font-new pl-10 text-5xl">capítol 1: Lluny del Bosc</h1>
       <iframe
         src="https://player.vimeo.com/video/664231814?h=391783c052&amp;badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
-        width="730"
-        height="411"
+        width="1000"
+        height="563"
         frameborder="0"
         allow="autoplay; fullscreen; picture-in-picture"
         allowfullscreen
-        title="Cap&amp;iacute;tol 1: Lluny del bosc"
-      >
-      </iframe>
-      <div class="bg-slate-50 h-4"></div>
-      <div class="bg-white h-10">comentario</div>
+        title="Cap&amp;iacute;tol 1: 
+Lluny del bosc"
+      ></iframe>
+      <div class="text-white">iconos</div>
+      <div class="container mx-auto mb-10">
+        <input type="text" placeholder="Nom" class="bg-white pl-4 py-1 rounded-lg self-start mt-10 mb-5">
+      <textarea name="comentaris" id="comentaris" placeholder="Deixa un comentari" cols="30" rows="10" class="w-full px-4 rounded-lg py-4"></textarea>
+      </div>
+
     </div>
   </div>
 </template>
